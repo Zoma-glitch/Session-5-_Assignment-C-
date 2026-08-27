edffe1f46c68f82741c188e88dbb29aeb8e5f720
Zoma-glitch <hazem.safwat.55@gmail.com>
2026-08-27 21:50:07 +0300

    Question04

--- Session 5 _Assignment C#/Program.cs
+++ Session 5 _Assignment C#/Program.cs
@@ -7,10 +7,18 @@ namespace Session_5__Assignment_C_
         static void Main(string[] args)
         {
             Book book = new Book();
-            book.Title = "Clean Code";
+
+            //book.Title = "Clean Code";
+
             //Console.WriteLine(book.Password);
+
             //Console.WriteLine(book.copiesInStock);
-            Console.WriteLine(book.Title);
+
+            //Console.WriteLine(book.Title);
+
+            book.Genre = Genre.Science;
+
+            Console.WriteLine(book.Genre);
         }
 
         #region Question01
@@ -38,6 +46,29 @@ namespace Session_5__Assignment_C_
 
         #region Question03
         //3-Add a public string Title; field to Book.Set it and print it from Main.
+        //class Book
+        //{
+        //    private string Password = "Secret";
+
+        //    internal int copiesInStock = 5;
+
+        //    public string Title;
+        //}
+
+
+
+        #endregion
+
+
+        #region Question04
+        //4-Declare an enum Genre { Fiction, NonFiction, Science }. Add a Genre property to Book, assign it Genre.Science, and print it.
+
+        enum Genre
+        {
+            Fiction,
+            NonFiction,
+            Science
+        }
         class Book
         {
             private string Password = "Secret";
@@ -45,11 +76,9 @@ namespace Session_5__Assignment_C_
             internal int copiesInStock = 5;
 
             public string Title;
+
+            public Genre Genre;
         }
-
-
-
         #endregion
-
 }
 }
