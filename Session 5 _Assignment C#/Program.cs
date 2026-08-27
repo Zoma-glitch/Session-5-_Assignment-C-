abedc25f1b8f747e038aa741bf56bf1ec449176e
Zoma-glitch <hazem.safwat.55@gmail.com>
2026-08-27 19:59:37 +0300

    Add project files.

---
+++ Session 5 _Assignment C#/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Session_5__Assignment_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
