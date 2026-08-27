4ac6e84fe655cb85ecbd14082e6af6ae093390c9
Zoma-glitch <hazem.safwat.55@gmail.com>
2026-08-27 21:32:49 +0300

    Question02

--- Session 5 _Assignment C#/Program.cs
+++ Session 5 _Assignment C#/Program.cs
@@ -7,17 +7,31 @@ namespace Session_5__Assignment_C_
         static void Main(string[] args)
         {
             Book book = new Book();
-            Console.WriteLine(book.Password);
+            //Console.WriteLine(book.Password);
+            Console.WriteLine(book.copiesInStock);
         }
 
         #region Question01
         //Add a private string password = "secret"; field to a Book class. Try to print it from Main(outside the class). What happens, and why?
         //Error becuase private
+        //class Book
+        //{
+        //    private string Password = "Secret";
+
+        //}
+        #endregion
+
+        #region Question02
+        //2-Add an internal int copiesInStock = 5; field to Book.Print it from Main.Does it compile? Why?
+
         class Book
         {
             private string Password = "Secret";
 
+            internal int copiesInStock = 5;
         }
+
+
         #endregion
-    }
+}
 }
