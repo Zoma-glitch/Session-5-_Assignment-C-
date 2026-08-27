3f228cdc70a28009f254d0ece886749955bb2140
Zoma-glitch <hazem.safwat.55@gmail.com>
2026-08-27 21:26:05 +0300

    Question01

--- Session 5 _Assignment C#/Program.cs
+++ Session 5 _Assignment C#/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Session_5__Assignment_C_
+﻿using System.Drawing;
+
+namespace Session_5__Assignment_C_
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Book book = new Book();
+            Console.WriteLine(book.Password);
         }
+
+        #region Question01
+        //Add a private string password = "secret"; field to a Book class. Try to print it from Main(outside the class). What happens, and why?
+        //Error becuase private
+        class Book
+        {
+            private string Password = "Secret";
+
+        }
+        #endregion
     }
 }
